f7c7c141d95c7bb6391cc7ec7f7ed5d550ea8eec
bcdroid <w1248153523@163.com>
2021-01-15 11:36:07 +0800

    1

---
+++ src/views/Info.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="info">
+<h1>hello, bcdroid</h1>
+<button type="button" @click="add()"></button>
+</div>
+</template>
+<script>
+export default {
+name: "Info",
+methods: {
+add() {
+console.log("Info执行了方法，按下了按钮")
+}
+}
+}
+</script>
